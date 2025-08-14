29269c577cd17d1c65fe3b1fc4e840733e8bb9fd
julioasdw <108040485+julioasdw@users.noreply.github.com>
2025-08-13 23:56:56 -0300

    atualizando ate js aula 164

--- CursoWEB/projetos/seção10.0_JavaScript-152-/meu_script.js
+++ CursoWEB/projetos/seção10.0_JavaScript-152-/meu_script.js
@@ -1,1 +1,20 @@
 alert('Olá, seja bem vindo ao curso!')
+document.getElementById('nome').value = ' Oi'
+
+//Srtring
+var texto = "curso de java script"
+//number
+var inteiro = 34
+var pontoFlutuante = 3.4
+//Boolean
+var boleano = false
+
+alert(texto)
+document.write(inteiro)
+console.log(boleano)
+
+//160. Concatenação_________________________________
+var nome = 'Júlio'
+var idade160 = 24
+document.write('<h4>Olá ' + nome + ', tudo  bem? Sua idade é : ' + idade160 + '</h4>')
+ 
