546aada26a83818cde96fc0978545f6c93eac3b7
julioasdw <108040485+julioasdw@users.noreply.github.com>
2025-09-09 23:54:18 -0300

    atualizando web

---
+++ seção13_Linguagem_de_Programação_PHP_(Hypertext Preprocessor)/index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>curso PHP</title>
+    </head>
+    <body>
+        <?php echo 'Utilizando a tag impressão'; ?>
+
+        <br>
+        <?= 'Utilizando a tag impressão' ?>
+        
+        <br>
+        <? echo 'Utilizando a tag curta' ?>
+        
+    </body>
+</html>
