481ac67be61d8679fdb163917a37a28db8c23abd
julioasdw <108040485+julioasdw@users.noreply.github.com>
2025-10-20 00:27:36 -0300

    315

--- seção11.0_ECMAScript 2015 (ES6)_e_Orientação_a_Objetos-244/app1/app.js
+++ seção11.0_ECMAScript 2015 (ES6)_e_Orientação_a_Objetos-244/app1/app.js
@@ -6,9 +6,67 @@ class Despesa {
         this.tipo = tipo
         this.descricao = descricao
         this.valor = valor
-        
+    }
+    
+    validarDados() {
+        for(let i in this) {
+            if(this[i] == undefined || this[i] == '' || this[i] == null || this[i] == NaN) {
+                return false
+            }
+        }
+        return true
+
     }
 }
+
+class Bd {
+    constructor() {
+        let id = localStorage.getItem('id')
+        console.log(id)
+        if(id === null) {
+            localStorage.setItem('id', 0)
+        }
+    }
+    getProximoId() {
+        let proximoId = localStorage.getItem('id')
+        return (parseInt(proximoId) + 1)
+    }
+
+    gravar(d) {
+        let id = this.getProximoId()
+        
+        localStorage.setItem(id, JSON.stringify(d))
+        
+        localStorage.setItem('id', id)
+    }
+
+    recuperarTodosRegistros() {
+        let despesas = Array()
+
+        let id = localStorage.getItem('id')
+
+        for(let i = 0; i <= id; i++) {
+            despesas.push(JSON.parse(localStorage.getItem(i)))
+        }
+
+        console.log(despesas)
+    }
+    
+}
+
+let bd = new Bd()
+
+// // como converter para JSON:
+// let pro = {
+//         a: 'nome', 
+//         b: 1700
+//     }
+//     JSON.stringify(pro)
+
+
+// como converter de JSON para OBJ:
+// let proJSON = '{"teste": "test", "b": "aaaa"}' // string JSON
+// let objeto = JSON.parse(proJSON)
 
 function cadastrarDespesa() {
     let ano = document.getElementById('ano')
@@ -18,14 +76,38 @@ function cadastrarDespesa() {
     let descricao = document.getElementById('descricao')
     let valor = document.getElementById('valor')
     
-    let despesa = new Despesa(
-        ano.value,
-        mes.value,
-        dia.value,
-        tipo.value,
-        descricao.value,
-        valor.value
-    )
+    let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
+    // console.log(despesa)
 
-    console.log(despesa)
+    // document.getElementById('exampleModalLabel1').innerText = 'TESTE'
+    // document.getElementById('infoModal').innerText = 'aaaaaaaa'
+    // document.getElementById('corBtnModal').classList.toggle('btn-success')
+
+
+
+    if(despesa.validarDados()) {
+        console.log('Dados validos!')
+        bd.gravar(despesa)
+        $('#registroDespesa').modal('show')
+
+        document.getElementById('cor_titulo_modal').className = 'modal-header text-success'
+        document.getElementById('exampleModalLabel1').innerText = 'Registro inserido com sucesso!'
+        document.getElementById('infoModal').innerText = 'Despesa cadastrada com sucesso!'
+        document.getElementById('corBtnModal').className = ('btn btn-success')
+        document.getElementById('corBtnModal').innerText = 'Voltar'
+        
+    } else {
+        console.log('Dados invalidos!')
+        $('#registroDespesa').modal('show')
+
+        document.getElementById('cor_titulo_modal').className = 'modal-header text-danger'
+        document.getElementById('exampleModalLabel1').innerText = 'Erro Gravação'
+        document.getElementById('infoModal').innerText = 'Existem campos brigatórios que não foram preenchidos!'
+        document.getElementById('corBtnModal').className = ('btn btn-danger')
+        document.getElementById('corBtnModal').innerText = 'Voltar e corrigir'
+    }
+}
+
+function carregaListaDespesas() {
+    bd.recuperarTodosRegistros()
 }
